35a186e87d1c3060c188273999966b8bfdddb936
amd64fox <62529699+amd64fox@users.noreply.github.com>
2022-12-23 16:18:05 +0300

    fixed incorrect path
    https://github.com/SpotX-CLI/SpotX-Win/issues/263

--- Uninstall-Spotify.bat
+++ Uninstall-Spotify.bat
@@ -3,29 +3,29 @@
 set a=0
 set b=0
 
-if exist %localappdata%\Spotify\Update (
-    icacls %localappdata%\Spotify\Update /reset /T > NUL 2>&1
+if exist "%localappdata%\Spotify\Update" (
+    icacls "%localappdata%\Spotify\Update" /reset /T > NUL 2>&1
 )
 
-if exist %appdata%\Spotify\Spotify.exe ( 
-    start "" /wait %appdata%\Spotify\Spotify.exe /UNINSTALL /SILENT
+if exist "%appdata%\Spotify\Spotify.exe" ( 
+    start "" /wait "%appdata%\Spotify\Spotify.exe" /UNINSTALL /SILENT
     set /a b=%a% + 1
 )
 
 TIMEOUT /T 1 > NUL 2>&1 
 
-if exist %appdata%\Spotify (
-    rd /s/q %appdata%\Spotify > NUL 2>&1
+if exist "%appdata%\Spotify" (
+    rd /s/q "%appdata%\Spotify" > NUL 2>&1
     set /a b=%a% + 1
 )
 
-if exist %localappdata%\Spotify (
-    rd /s/q %localappdata%\Spotify > NUL 2>&1
+if exist "%localappdata%\Spotify" (
+    rd /s/q "%localappdata%\Spotify" > NUL 2>&1
     set /a b=%a% + 1
 )
 
-if exist %temp%\SpotifyUninstall.exe (
-    del /s /q  %temp%\SpotifyUninstall.exe > NUL 2>&1
+if exist "%temp%\SpotifyUninstall.exe" (
+    del /s /q  "%temp%\SpotifyUninstall.exe" > NUL 2>&1
     set /a b=%a% + 1
 )
 
