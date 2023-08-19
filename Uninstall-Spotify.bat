2fac2364e128c04fc6b33de50ff7e0d3323f3245
Relvin Arsenio <88470966+RelvinArsenio@users.noreply.github.com>
2023-08-19 12:46:10 +0700

    Enhance uninstallation script and improve comments
    Details of Changes:
    - Added delayed expansion using `setlocal enabledelayedexpansion` to allow variables to be updated within loops and blocks.
    - Corrected variable usage for accurate counting of removal operations.
    - Improved error handling during Spotify uninstallation process.
    - Utilized a loop to remove Spotify data folders from both AppData roaming and local AppData.
    - Improved deletion process for "SpotifyUninstall.exe" if present.
    - Enhanced comments to provide clearer explanations of code sections.

--- Uninstall-Spotify.bat
+++ Uninstall-Spotify.bat
@@ -1,34 +1,41 @@
 @echo off
 
-set a=0
-set b=0
+setlocal enabledelayedexpansion
 
+REM Reset permissions for Spotify Update folder
 if exist "%localappdata%\Spotify\Update" (
     icacls "%localappdata%\Spotify\Update" /reset /T > NUL 2>&1
 )
 
-if exist "%appdata%\Spotify\Spotify.exe" ( 
+set a=0
+set b=0
+
+REM Uninstall Spotify if it exists
+if exist "%appdata%\Spotify\Spotify.exe" (
     start "" /wait "%appdata%\Spotify\Spotify.exe" /UNINSTALL /SILENT
-    set /a b=%a% + 1
+    set /a b=!b! + 1
 )
 
-timeout /t 1 > NUL 2>&1 
+timeout /t 1 > NUL 2>&1
 
-if exist "%appdata%\Spotify" (
-    rd /s/q "%appdata%\Spotify" > NUL 2>&1
-    set /a b=%a% + 1
+REM Remove Spotify data folders
+for %%d in ("%appdata%\Spotify" "%localappdata%\Spotify") do (
+    if exist "%%d" (
+        rd /s/q "%%d" > NUL 2>&1
+        set /a b=!b! + 1
+    )
 )
 
-if exist "%localappdata%\Spotify" (
-    rd /s/q "%localappdata%\Spotify" > NUL 2>&1
-    set /a b=%a% + 1
-)
-
+REM Delete SpotifyUninstall.exe if it exists
 if exist "%temp%\SpotifyUninstall.exe" (
     del /s /q  "%temp%\SpotifyUninstall.exe" > NUL 2>&1
-    set /a b=%a% + 1
+    set /a b=!b! + 1
 )
 
-if %b% == 0 echo Spotify not found
-if %b% NEQ 0 echo Spotify removed
+if !b! == 0 (
+    echo Spotify not found
+) else (
+    echo Spotify removed
+)
+
 pause & exit
