079e1b9bbd9e52734ecaf032987db1e04d2aa509
cloudskies <113025487+cloudcloudskies@users.noreply.github.com>
2024-04-05 19:35:08 +0300

    Optimize the entire batch script

--- Uninstall-Spotify.bat
+++ Uninstall-Spotify.bat
@@ -1,11 +1,8 @@
 @echo off
-
 setlocal enabledelayedexpansion
 
 REM Reset permissions for Spotify Update folder
-if exist "%localappdata%\Spotify\Update" (
-    icacls "%localappdata%\Spotify\Update" /reset /T > NUL 2>&1
-)
+if exist "%localappdata%\Spotify\Update" icacls "%localappdata%\Spotify\Update" /reset /T > NUL 2>&1
 
 set actions=0
 
@@ -13,9 +10,8 @@ REM Uninstall Spotify if it exists
 if exist "%appdata%\Spotify\Spotify.exe" (
     start "" /wait "%appdata%\Spotify\Spotify.exe" /UNINSTALL /SILENT
     set /a actions+=1
+    timeout /t 1 > NUL 2>&1
 )
-
-timeout /t 1 > NUL 2>&1
 
 REM Remove Spotify data folders
 for %%d in ("%appdata%\Spotify" "%localappdata%\Spotify") do (
@@ -31,10 +27,10 @@ if exist "%temp%\SpotifyUninstall.exe" (
     set /a actions+=1
 )
 
-if !actions! == 0 (
-    echo Spotify is not installed or not found
-) else (
-    echo Spotify has been successfully uninstalled
+echo %actions% | findstr "^0$" > nul && (
+    echo Spotify is not installed or not found.
+) || (
+    echo Spotify has been successfully uninstalled.
 )
 
 pause & exit
