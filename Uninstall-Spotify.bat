b0da09de8603c8c67452abc3c734acbf9a1a7535
cloudskies <113025487+cloudcloudskies@users.noreply.github.com>
2024-04-05 19:35:40 +0300

    Add . at the end of the comments

--- Uninstall-Spotify.bat
+++ Uninstall-Spotify.bat
@@ -1,19 +1,19 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Reset permissions for Spotify Update folder
+REM Reset permissions for Spotify Update folder.
 if exist "%localappdata%\Spotify\Update" icacls "%localappdata%\Spotify\Update" /reset /T > NUL 2>&1
 
 set actions=0
 
-REM Uninstall Spotify if it exists
+REM Uninstall Spotify if it exists.
 if exist "%appdata%\Spotify\Spotify.exe" (
     start "" /wait "%appdata%\Spotify\Spotify.exe" /UNINSTALL /SILENT
     set /a actions+=1
     timeout /t 1 > NUL 2>&1
 )
 
-REM Remove Spotify data folders
+REM Remove Spotify data folders.
 for %%d in ("%appdata%\Spotify" "%localappdata%\Spotify") do (
     if exist "%%d" (
         rd /s/q "%%d" > NUL 2>&1
@@ -21,7 +21,7 @@ for %%d in ("%appdata%\Spotify" "%localappdata%\Spotify") do (
     )
 )
 
-REM Delete SpotifyUninstall.exe if it exists
+REM Delete SpotifyUninstall.exe if it exists.
 if exist "%temp%\SpotifyUninstall.exe" (
     del /s /q  "%temp%\SpotifyUninstall.exe" > NUL 2>&1
     set /a actions+=1
