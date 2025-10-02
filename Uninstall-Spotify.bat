65b6e570665d18c66246e6ce164d576c5a99de31
amd64fox <62529699+amd64fox@users.noreply.github.com>
2025-10-02 19:36:42 +0300

    attempting to fix a frozen cmd window

--- Uninstall-Spotify.bat
+++ Uninstall-Spotify.bat
@@ -1,5 +1,4 @@
 @echo off
-
 set PWSH="%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command
 set RawGit='https://raw.githubusercontent.com/amd64fox/Uninstall-Spotify/refs/heads/main/core.ps1'
 set JsDelivr='https://cdn.jsdelivr.net/gh/amd64fox/Uninstall-Spotify@refs/heads/main/core.ps1'
@@ -12,15 +11,15 @@ if "%1"=="e" goto :run
 if %errorLevel% == 0 (
     goto :run
 ) else (
-    %PWSH% "try { start wt 'cmd /c \"%~dpnx0\" e' -v RunAs } catch { start cmd '/c \"%~dpnx0\" e' -v RunAs}"
-    exit /b
+    %PWSH% "try { start wt 'cmd /c \"\"%~dpnx0\" e\"' -v RunAs } catch { start cmd '/c \"\"%~dpnx0\" e\"' -v RunAs }"
+    exit
 )
 
 :run
 if exist "%~dp0core.ps1" (
-    %PWSH% "& { $(Get-Content '%~dp0core.ps1' -Raw) } | iex"
+    %PWSH% "& { $(Get-Content '%~dp0core.ps1' -Raw) } | iex; exit $LASTEXITCODE"
 ) else (
-    %PWSH% %TLS% "& { $(try { iwr -useb %RawGit% } catch { iwr -useb %JsDelivr% }) } | iex"
+    %PWSH% %TLS% "& { $(try { iwr -useb %RawGit% } catch { iwr -useb %JsDelivr% }) } | iex; exit $LASTEXITCODE"
 )
 
-exit /b
+exit
